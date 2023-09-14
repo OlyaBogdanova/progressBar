d29a1646e38ce939622b411404243fa8b4ccc9bc
Olga Bogdanova <bon@incom-sr.ru>
2023-09-14 14:24:06 +0300

    first commit

--- script.js
+++ script.js
@@ -1,52 +1,9 @@
-const prevBtn = document.getElementById("prev");
-const nextBtn = document.getElementById("next");
-const circleList = document.querySelectorAll(".circle");
-const progress = document.getElementById("progress");
+const openBtn = document.getElementById("open");
+const closeBtn = document.getElementById("close");
+const container = document.querySelector(".container");
 
-let activeStep = 1;
-const maxStep = circleList.length;
+openBtn.addEventListener('click', ()=>container.classList.add('show-nav'))
+closeBtn.addEventListener('click', ()=>container.classList.remove('show-nav'))
 
-prevBtn.addEventListener("click", () => {
-  if (activeStep !== 1) {
-    activeStep--;
 
-    update();
-    checkBtnState();
-  }
-});
 
-nextBtn.addEventListener("click", () => {
-  if (activeStep < maxStep) {
-    activeStep++;
-    update();
-    checkBtnState();
-  }
-});
-
-function update() {
-  circleList.forEach((circle, index) => {
-    if (index < activeStep) {
-      circle.classList.add("active");
-    } else {
-      circle.classList.remove("active");
-    }
-  });
-  changeProgressWidth();
-}
-
-function changeProgressWidth() {
-  const actives = document.querySelectorAll(".active");
-  const progressWidth = ((actives.length - 1) / (circleList.length - 1)) * 100;
-  progress.style.width = `${progressWidth}%`;
-}
-
-function checkBtnState() {
-  if (activeStep === 1) {
-    prevBtn.disabled = true;
-  } else if (activeStep === circleList.length) {
-    nextBtn.disabled = true;
-  } else {
-    prevBtn.disabled = false;
-    nextBtn.disabled = false;
-  }
-}
